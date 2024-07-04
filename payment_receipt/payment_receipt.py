bd0a524b8ccbaf5b7bca567f1ba3ed8e7549700e
kannoth <kannoth1997house@gmail.com>
2024-07-04 22:24:25 +0530

    commit2

--- payment_receipt/payment_receipt.py
+++ payment_receipt/payment_receipt.py
@@ -1,4 +1,8 @@
-
+"""
+CREATING PAYMENT RECEIPT
+Creating payment receipts is a pretty common task, be it an e-commerce website or any local store for that matter.
+Here, you have to create our own transaction receipts just by using python. We would be using reportlab to generate the PDFs. Generally, it comes as a built-in package but sometimes it might not be present too. If it is not present, then simply type the following in your terminal
+"""
 
 import tkinter as tk
 from tkinter import messagebox
